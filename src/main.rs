fe2f26f9f8f62f517f6fef5bd39ffa79f5aef4f2
Kartmaan <cartmanboss69@gmail.com>
2024-10-17 15:17:18 +0200

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use std::io;
-use rand::Rng;
+use std::io; // Entrée utilisateur
+use rand::Rng; // Nombres aléatoires
 
+/// Les variantes de cet enum représentent toutes les valeurs
+/// discrètes possibles des comparaisons numériques entre le
+/// nombre inséré par l'utilisateur et le nombre aléatoire
+/// généré
 enum GuessResult {
     Greater,
     Less,
@@ -21,7 +25,7 @@ enum GuessResult {
 /// let num1 = 15;
 /// let num2 = 22;
 /// let compare = num1.cmp(&num2)
-/// 'let compare' contient : std::cmp::Ordering::Less
+/// 'let compare' contient maintenant : std::cmp::Ordering::Less
 fn equality_match(guessed_num: i32, rnd_num: i32) -> GuessResult {
     match guessed_num.cmp(&rnd_num) {
         std::cmp::Ordering::Less => GuessResult::Greater,
@@ -35,24 +39,31 @@ fn main() {
     let mut attempts: i32 = 0;
 
     // Génération du nombre aléatoire
+    // Un nombre entier compris entre 1 (compris) et 100 (compris)
     let rnd_num: i32 = rand::thread_rng().gen_range(1..=100);
 
     // La boucle continue tant que true
     let mut in_guess: bool = true;
 
-    // Création d'un String pour stocker l'entrée utilisateur
+    // Création d'un String vide pour stocker l'entrée utilisateur
     let mut input: String = String::new();
 
+    // Début de la boucle
     while in_guess {
         input.clear(); // L'entrée est effacée
         println!("Essayez de deviner le nombre entre 0 et 100 :");
 
         // Lecture de l'entrée utilisateur
+        // read_line() : toute l'entrée, jusqu'à la pression de 
+        // la touche Entrée, sera enregistrée dans 'input'
         io::stdin()
             .read_line(&mut input)
             .expect("Failed to read");
 
         // Conversion de l'entrée en nombre
+        // trim() : supprime les espaces
+        // parse() : convertie selon le type de la variable assignée
+        // expect() : Message d'erreur en cas d'erreur
         let num_guessed: i32 = input.trim()
             .parse()
             .expect("Please type a valid number");
@@ -62,6 +73,8 @@ fn main() {
         // La fonction renvoi une variante de l'enum GuessResult
         let guess:GuessResult = equality_match(num_guessed, rnd_num);
 
+        // Instructions à executer selon le résultat de la 
+        // comapraison
         match guess {
             // C'est plus petit
             GuessResult::Less => {
